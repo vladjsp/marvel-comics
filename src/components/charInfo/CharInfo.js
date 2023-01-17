31814bb7a28e92964285b8ad61e42640c7fee377
Vlad <me@mypc.acer>
2023-01-17 12:52:49 +0200

    after lesson 152

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -76,12 +76,21 @@ class CharInfo extends Component {
 
 const View = ({ char }) => {
   const { name, description, thumbnail, homepage, wiki, comics } = char;
+
+  let imgStyle = { objectFit: 'cover' };
+  if (
+    thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg'
+  ) {
+    imgStyle = { objectFit: 'contain' };
+  }
+
   return (
     <>
       <div className='char__basics'>
         <img
           src={thumbnail}
           alt={name}
+          style={imgStyle}
         />
         <div>
           <div className='char__info-name'>{name}</div>
@@ -104,7 +113,10 @@ const View = ({ char }) => {
       <div className='char__descr'>{description}</div>
       <div className='char__comics'>Comics:</div>
       <ul className='char__comics-list'>
+        {comics.length > 0 ? null : 'There are no comisc for this character.'}
         {comics.map((item, i) => {
+          // eslint-disable-next-line
+          if (i >= 10) return;
           return (
             <li
               key={i}
